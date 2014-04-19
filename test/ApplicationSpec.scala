6bfe91df3ef6978d1b0da544a7830bbe20aaf2aa
enijkamp <erik.nijkamp@gmail.com>
2014-04-20 00:24:28 +0200

    spec2 test for salat, salat not working yet

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -6,7 +6,6 @@ import play.api.test._
 import play.api.test.Helpers._
 
 /**
- * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
