1c8262be4714a6a4a09af9fd22416d81d0cfc880
enijkamp <erik.nijkamp@gmail.com>
2014-04-20 14:55:11 +0200

    fixture fixed

--- test/ModelSpec.scala
+++ test/ModelSpec.scala
@@ -3,29 +3,34 @@ import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
 
+import com.mongodb.casbah.Imports._
+
 import play.api.test._
 
 @RunWith(classOf[JUnitRunner])
 class ModelSpec extends Specification {
 
-  def withUser(test: (User) => Any) {
-    val user = User(username = "user", password = "pass")
-    User.insert(user)
-    try test(user)
-    finally User.remove(user)
-  }
-
   "A user" should {
-    "be findable by his username" in new WithApplication { user =>
-      User.findOneByUsername("user").get.username must be equalTo "user"
+    "be retrieved by his username" in new WithApplication {
+      withUser {
+        user =>
+          User.findOneByUsername(user.username).get must be equalTo user
+      }
     }
   }
 
   "A nil user" should {
     "not be retrievable" in new WithApplication {
-      User.remove(User(username = "user", password = "pass"))
-      User.findOneByUsername("user") must be equalTo None
+      User.remove(User(username = "nil", password = "pass"))
+      User.findOneByUsername("nil") must be equalTo None
     }
   }
 
+  def withUser(test: (User) => Any) {
+    val user = User(username = "user", password = "pass")
+    User.remove(MongoDBObject("username" -> user.username))
+    User.insert(user)
+    try test(user)
+    finally User.remove(user)
+  }
 }
