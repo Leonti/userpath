32121d02ccf1c5c392f45cbb07ce5cb997419cf8
enijkamp <erik.nijkamp@gmail.com>
2014-04-20 14:33:50 +0200

    adding cool loan-fixtures to db test

--- test/ModelSpec.scala
+++ test/ModelSpec.scala
@@ -4,21 +4,27 @@ import org.specs2.runner._
 import org.junit.runner._
 
 import play.api.test._
-import play.api.test.Helpers._
 
 @RunWith(classOf[JUnitRunner])
 class ModelSpec extends Specification {
 
+  def withUser(test: (User) => Any) {
+    val user = User(username = "user", password = "pass")
+    User.insert(user)
+    try test(user)
+    finally User.remove(user)
+  }
+
   "A user" should {
-    "be findable by his username" in new WithApplication {
-      User.insert(User(username = "user", password = "pass"))
+    "be findable by his username" in new WithApplication { user =>
       User.findOneByUsername("user").get.username must be equalTo "user"
     }
   }
 
-  "A non-existing user" should {
+  "A nil user" should {
     "not be retrievable" in new WithApplication {
-      User.findOneByUsername("foobar") must be equalTo None
+      User.remove(User(username = "user", password = "pass"))
+      User.findOneByUsername("user") must be equalTo None
     }
   }
 
